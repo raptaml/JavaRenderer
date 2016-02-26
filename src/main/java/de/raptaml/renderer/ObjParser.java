55548a0806c1f509b90c421d28fc98530a95b10b
Matthias Lemmer <matthias.lemmer@kks.uni-marburg.de>
2016-02-26 12:04:10 +0100

    parsing arrays

--- src/main/java/de/raptaml/renderer/ObjParser.java
+++ src/main/java/de/raptaml/renderer/ObjParser.java
@@ -6,8 +6,6 @@
 package de.raptaml.renderer;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -38,25 +36,62 @@ public class ObjParser {
     
     void parseObj() {
         
-        Obj obj = new Obj();
+        Obj obj;
         ArrayList<String> vertStrings = new ArrayList<>();
         ArrayList<String> faceStrings = new ArrayList<>();
+        int totalVerts =0;
+        int totalFaces = 0;
         String line;
+        
+        long then = System.currentTimeMillis();
         try {
             System.out.println(objFile.getEncoding());
             while ((line = reader.readLine()) != null)  {
-                //System.out.println(line.charAt(2));
                 if ((line.length() > 0) && line.charAt(0) == 'v' && line.charAt(1) == ' ') {
                     String[] splittedLine = line.split(" ");
                     for (int i = 1; i < splittedLine.length; i++)
                         vertStrings.add(splittedLine[i]);
                 } else if ((line.length() > 0) && line.charAt(0) == 'f' && line.charAt(1) == ' ') {
-                    
-                }
-                
-                        
+                    String[] splittedLine = line.split("f");
+                    faceStrings.add(splittedLine[1].trim());
+                } else if ((line.length() > 0) && (line.startsWith("#")) && line.contains("vertices")) {
+                    if (!line.contains("texture"))
+                        totalVerts = Integer.valueOf(line.split(" ")[1]);           
+                } else if ((line.length() > 0) && (line.startsWith("#")) && line.contains("faces")) {
+                    totalFaces = Integer.valueOf(line.split(" ")[1]);        
+                }      
             }
-            System.out.println(vertStrings);
+            
+            long now = System.currentTimeMillis();
+
+            System.out.println(now - then);
+          
+            //build vert and face array from splitted strs
+            obj = new Obj(totalVerts, totalFaces);
+            int n = 0;
+            for (float[] vert : obj.verts) {
+                for (int j = 0; j < 3; j++) {
+                    vert[j] = Float.valueOf(vertStrings.get(n));  
+                    n++;
+                }
+            }
+            n= 0;
+            System.out.println(obj.faces.length +" "+faceStrings.size());
+            for (int[] face : obj.faces) {
+                for (String faceString :faceStrings.get(n).split(" ")) {
+                    for (int j = 0; j < 3; j++) {
+                        face[j] = Integer.valueOf(faceString.split("/")[j]);
+                    //face[j] = Integer.valueOf(faceStrings.get(n).split("/")[j*3].trim());  
+                    }
+                n++;
+                if (n==2493)
+                    System.out.println(n);
+                }
+            }
+            //DBG
+            for (int[] face : obj.faces) {
+                System.out.println(Arrays.toString(face));
+            }
             
         } catch (IOException ex) {
             Logger.getLogger(ObjParser.class.getName()).log(Level.SEVERE, null, ex);
@@ -65,12 +100,16 @@ public class ObjParser {
         
     }
     public class Obj {
-        public Obj() {
-            
+        
+        float verts[][];
+        int faces[][];
+        
+        public Obj(int vertCount, int faceCount) {
+            this.verts = new float[vertCount][3];
+            this.faces = new int[faceCount][3];
         }
     
-        float vert[];
-        int faces[][];
+
     }
 }
 
